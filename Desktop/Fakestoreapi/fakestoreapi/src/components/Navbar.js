ec10675fb3f503dc4eebcc30e0a6e3b78f6c5774
VijayaDharshiniSivakumar <vijayadharshinisivakumar04@gmail.com>
2024-07-19 12:32:15 +0530

    Initial commit

---
+++ Desktop/Fakestoreapi/fakestoreapi/src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../App.css'
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+      <h2>Shopping App</h2>
+    </div>
+  )
+}
+
+export default Navbar
